15c8f07c36a5412dc8ea716259a0615ff4b23211
KVexcavator <str_token@mail.ru>
2020-06-25 18:02:07 +0300

    add methods to Dish model: to parametrize names, to average score. seed dates

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,2 +1,13 @@
 class Dish < ApplicationRecord
+  has_many :rewiews
+
+  before_create :slugify
+  
+  def slugify
+    self.slug = name.parameterize
+  end
+
+  def avg_score
+    rewiews.average(:score).round(2).to_f
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+dishes = Dish.create([
+  {
+    name: "Fried chicken" ,
+    image_url: "https://png.pngtree.com/png-clipart/20190921/original/pngtree-a-bowl-of-hot-roast-chicken-free-illustration-png-image_4715992.jpg"
+  },
+  {
+    name: "Plastic sandwich",
+    image_url: "https://png.pngtree.com/png-clipart/20200224/original/pngtree-burger-vector-illustration-with-simple-black-line-design-burger-icon-fast-png-image_5225332.jpg"
+  },
+  {
+    name: "smoked fish",
+    image_url: "https://png.pngtree.com/png-clipart/20190925/original/pngtree-grilled-fish-in-a-dish-mapped-photograph-png-and-eps-png-image_4961795.jpg"
+  }
+])
+
+rewiews = Rewiew.create([
+  {
+    title: "Great dish",
+    description: "I had a lovely time,",
+    score: 5,
+    dish: dishes.first
+  },
+  {
+    title: "Bad dish",
+    description: "I had a bad time.",
+    score: 1,
+    dish: dishes.first
+  }
+])
