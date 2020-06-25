2a317a5902d40810bb51e1e8a072fa87a843c4c6
KVexcavator <str_token@mail.ru>
2020-06-25 21:15:40 +0300

    create rewiews controller

--- app/controllers/api/v1/dishes_controller.rb
+++ app/controllers/api/v1/dishes_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class DishesController < ApplicationController
+      protect_from_forgery with: :null_session
       
       def index
         dishes = Dish.all
--- app/controllers/api/v1/rewiews_controller.rb
+++ app/controllers/api/v1/rewiews_controller.rb
@@ -1,7 +1,31 @@
 module Api
   module V1
     class RewiewsController < ApplicationController
+      protect_from_forgery with: :null_session
 
+      def create
+        rewiew = Rewiew.new(rewiew_params)
+        if rewiew.save
+          render json: RewiewSerializer.new(rewiew).serialized_json
+        else
+          render json: { error: rewiew.errors.messages }, status: 422
+        end
+      end
+
+      def destroy 
+        rewiew = Rewiew.find(params[:id])
+        if rewiew.destroy 
+          head :no_content
+        else
+          render json: { error: rewiew.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      def rewiew_params
+        params.require(:rewiew).permit(:title, :description, :score, :dish_id)
+      end
     end
   end
 end
