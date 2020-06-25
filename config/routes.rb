a2b8f2ca30c4476ea08e82ce2e7f69388bcab94e
KVexcavator <str_token@mail.ru>
2020-06-25 19:40:43 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
+  namespace :api do
+    namespace :vl do
+      resources :dishes, param: :slug
+      resources :rewiews, only: [:create, :destroy]
+    end
+  end
+
+  get '*path', to: 'pages#index', via: :all
 end
