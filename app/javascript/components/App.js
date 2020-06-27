76a7cb3debcc56612682a82ce7c144243f87d1fe
KVexcavator <str_token@mail.ru>
2020-06-27 22:06:50 +0300

    add rails routes with react-router-dom

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Dishes from './Dishes/Dishes'
+import Dish from './Dish/Dish'
 
 const App = () => {
-  return (<div>Hello World</div>)
+  return (
+    <Switch>
+        <Route exact path="/" component={Dishes} />
+        <Route exact path="/dishes/:slug" component={Dish} />
+    </Switch>
+  )
 }
 
 export default App
---
+++ app/javascript/components/Dishes/Dishes.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Dishes = () => {
+  return (<div>This is Dishes#index view for our app.</div>)
+}
+
+export default Dishes
