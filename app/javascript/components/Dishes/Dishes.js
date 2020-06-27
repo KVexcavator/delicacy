281a27e5d845f122a8066873c58e8f0f402fc4c1
KVexcavator <str_token@mail.ru>
2020-06-27 22:55:03 +0300

    get list of dishes with axios

--- app/javascript/components/Dishes/Dishes.js
+++ app/javascript/components/Dishes/Dishes.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
 
 const Dishes = () => {
-  return (<div>This is Dishes#index view for our app.</div>)
+  const [dishes, setDishes] = useState([])
+
+  useEffect(() => {
+    // Get all of our dishes from api
+    // Update dishes in our state
+    axios.get('/api/v1/dishes.json')
+      .then( resp => {
+        setDishes(resp.data.data )
+      } )
+    .catch( resp => console.log(resp) )
+  }, [dishes.length])
+
+  const list = dishes.map( item => {
+    return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+  } )
+
+  return (
+    <Fragment>
+      <div>This is Dishes#index view for our app.</div>
+      <ul>{list}</ul>
+    </Fragment>
+  )
 }
 
 export default Dishes
